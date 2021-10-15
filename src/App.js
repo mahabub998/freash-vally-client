f721e54ebd565b1138da0a815e543245db2e51d5
mahabub alam <mahabu998@gmail.com>
2021-10-15 11:59:32 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,66 +1,51 @@
-import React, { createContext, useState } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import NoMatch from './Components/NoMatch/NoMatch';
-import Home from './Components/Home/Home';
-import Header from './Components/Header/Header';
-import AddEvents from './Components/AddEvents/AddEvents';
-import CheckOut from './Components/CheckOut/CheckOut';
-import Login from './Components/Login/Login';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import ManageProducts from './Components/ManageProducts/ManageProducts';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
+import React, { createContext, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NoMatch from "./Components/NoMatch/NoMatch";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import AddEvents from "./Components/AddEvents/AddEvents";
+import CheckOut from "./Components/CheckOut/CheckOut";
+import Login from "./Components/Login/Login";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import ManageProducts from "./Components/ManageProducts/ManageProducts";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
-
-
-
-export const  UserContext = createContext();
-
+export const UserContext = createContext();
 
 function App() {
- const  [loggedInUser,setLoggedInUser] = useState({});
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-<UserContext.Provider value ={ [loggedInUser,setLoggedInUser]}>
- 
-    <Router>
-    
-      <Navbar/>
-      <Switch>
-        <Route path="/Home"> 
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/addEvents">
-          <AddEvents></AddEvents>
-        </Route>
-        {/*<PrivateRoute path="/CheckOut/:productId">
-         <CheckOut></CheckOut>
-  </PrivateRoute>*/}
-  <Route path="/CheckOut/:productId">
-         <CheckOut></CheckOut>
-        </Route>
-        <Route path="/login">
-      <Login />
-        </Route>
-      
-  <Route path="/ManageProducts">
-         <ManageProducts></ManageProducts>
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/addEvents">
+            <AddEvents></AddEvents>
+          </Route>
+          <PrivateRoute path="/CheckOut/:productId">
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/ManageProducts">
+            <ManageProducts></ManageProducts>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </UserContext.Provider>
-   
   );
 }
 
--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -1,53 +1,58 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import swal from 'sweetalert';
-import { Card } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import './CheckOut.css'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import swal from "sweetalert";
+import { Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import "./CheckOut.css";
 
 const CheckOut = ({ event }) => {
-    const [products, setProducts] = useState([])
-    console.log(products)
-    const { productId } = useParams();
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  const { productId } = useParams();
 
-    useEffect(() => {
-        fetch('http://localhost:5000/event/' + productId)
-            .then(res => res.json())
-            .then(data => setProducts(data))
+  useEffect(() => {
+    fetch("http://localhost:5000/event/" + productId)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [productId]);
 
-    }, [productId])
+  const handelClick = () => {
+    axios
+      .post("http://localhost:5000/addOrder")
+      .then((response) => {
+        response.data &&
+          swal(
+            "Order placed successfully",
+            "Your order placed successfully!",
+            "success"
+          );
+      })
+      .catch((error) => console.log(error));
+  };
 
-    const handelClick = () => {
-
-        axios.post('http://localhost:5000/addOrder',)
-            .then(response => {
-                response.data && swal("Order placed successfully", "Your order placed successfully!", "success");
-            })
-            .catch(error => console.log(error));
-    }
-
-    return (
-
-        <section className="card_position">
-           
-            <Card style={{ width: '25rem', height: '5rem' }} >
-                <Card.Img style={{ width: '100px', height: '100px' }} variant="top" src={products.imageUrl} />
-                <Card.Body >
-                    <Card.Title>{products.name}</Card.Title>
-                    <p>price : {products.price}</p>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-
-                    </Card.Text>
-                    <Button variant="primary" onClick={handelClick}>Buy Now</Button>
-                </Card.Body>
-            </Card>
-
-        </section>
-    );
+  return (
+    <section className="card_position">
+      <Card style={{ width: "25rem", height: "5rem" }}>
+        <Card.Img
+          style={{ width: "100px", height: "100px" }}
+          variant="top"
+          src={products.imageUrl}
+        />
+        <Card.Body>
+          <Card.Title>{products.name}</Card.Title>
+          <p>price : {products.price}</p>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary" onClick={handelClick}>
+            Buy Now
+          </Button>
+        </Card.Body>
+      </Card>
+    </section>
+  );
 };
 
 export default CheckOut;
